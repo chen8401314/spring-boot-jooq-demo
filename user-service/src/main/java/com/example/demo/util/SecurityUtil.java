b394c8e5e140a028fafe5eb4ba4b7eacccf18a12
chenxiang <328692993@qq.com>
2023-03-24 17:31:07 +0800

    [新增功能](master): 删除无效代码

--- user-service/src/main/java/com/example/demo/util/SecurityUtil.java
+++ user-service/src/main/java/com/example/demo/util/SecurityUtil.java
@@ -4,47 +4,12 @@ import com.huagui.common.base.context.OperationException;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
-import java.util.Random;
-import java.util.UUID;
 
 public class SecurityUtil {
-
-    private static Random random = new Random();
-
-    /**
-     * 6位安全码
-     *
-     * @return
-     */
-    public static String getMask() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 6; i++) {
-            int temp = random.nextInt(10);
-            sb.append(temp);
-        }
-        return sb.toString();
-    }
-
-    /**
-     * 生成用户token工具类
-     *
-     * @return
-     */
-    public static String getUUID() {
-        return UUID.randomUUID().toString().replace("-", "");
-    }
-
-    public static String getAccessToken() {
-        return getUUID();
-    }
 
     private static final char[] HEX_DIGITS = {'0', '1', '2', '3', '4', '5',
             '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
-    // 计算并获取CheckSum
-    public static String sign(String appId, String appSecret, String currentTime, String nonce) {
-        return encode("sha1", appId + appSecret + currentTime + nonce);
-    }
 
     // 计算并获取md5值
     public static String getMD5(String requestBody) {
