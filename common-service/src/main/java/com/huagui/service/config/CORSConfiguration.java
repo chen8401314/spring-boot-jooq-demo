5951ce417a5d3db3268863d77a42ea209d2be12c
chenxiang <328692993@qq.com>
2023-05-10 14:13:25 +0800

    [新增功能](master): 修复swagger

--- common-service/src/main/java/com/huagui/service/config/CORSConfiguration.java
+++ common-service/src/main/java/com/huagui/service/config/CORSConfiguration.java
@@ -3,9 +3,10 @@ package com.huagui.service.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CORSConfiguration extends WebMvcConfigurationSupport {
+public class CORSConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
